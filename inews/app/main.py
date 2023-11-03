cefa2620031da3cd237a5d5e89ced8416f81b567
Simon Alibert <alibert.sim@gmail.com>
2023-11-03 04:45:39 +0100

    :fire: Moved code to main and removed pipeline.py

--- inews/app/main.py
+++ inews/app/main.py
@@ -1,26 +1,52 @@
-from inews.domain import pipeline
+from inews.domain.models import ChannelList, Newsletter, StoryList, SummaryList, VideoList
+from inews.infra import io
+
+data_config = io.get_data_config()
+
+
+def build_channels(from_file: bool = True) -> ChannelList:
+    if from_file:
+        channels = ChannelList.init_from_file()
+    else:
+        channels_id = io.get_channels_id()
+        channels = ChannelList.init_from_api_with_ids(channels_id)
+    return channels
 
 
 def main():
     # getting channels
-    channels = pipeline.build_channels(from_file=True)
-    channels.update_last_week_videos()
+    channels = build_channels(from_file=True)
+    # channels.update_recent_videos()
     channels.save()
 
     # getting transcripts
-    videos = pipeline.build_videos(channels, from_file=True)
+    videos = VideoList.init_from_channels(channels, use_local_files=True)
     videos.get_available_transcripts()
     videos.drop_no_transcripts()
     videos.save()
 
-    # getting summaries
-    summaries = pipeline.build_summaries(videos, from_file=True)
-    summaries.get_all(videos)
+    # getting base summaries
+    summaries = SummaryList.init_from_videos(videos, use_local_files=True)
+    summaries.get_bases_from_videos(videos)
+    summaries.get_topics()
     summaries.save()
 
-    # summaries = pipeline.select_stories(summaries)
+    # # selecting stories
+    summaries.drop_irrelevants()
+    stories = StoryList.init_from_summaries(summaries, use_local_files=True)
+    # stories = StoryList.init_from_all_files()
+    stories.save()
 
-    # _ = pipeline.build_newsletters(summaries)
+    # # getting the rest of the content
+    stories.get_shorts_from_summaries(summaries)
+    stories.get_titles_from_summaries(summaries)
+    stories.get_user_groups_from_summaries(summaries)
+    stories.save()
+
+    for group_id in data_config["user_groups"]:
+        newsletter = Newsletter.init_from_summaries(group_id, stories)
+        newsletter.make_html()
+        newsletter.save()
 
 
 if __name__ == "__main__":
